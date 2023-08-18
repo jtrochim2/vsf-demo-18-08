98d5fb4af470e024b6faed11d8c8337e6ffdadaf
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-08-18 12:36:02 +0200

    feat: add sap sdk (#7)

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -1,10 +1,23 @@
+import { contentfulModule, ContentfulModuleType } from '@vsf-enterprise/contentful-sdk';
+import { sapccModule, SAPCCModuleType } from '@vsf-enterprise/sapcc-sdk';
 import { initSDK, buildModule } from '@vue-storefront/sdk';
 import { type SdkModule, sdkModule } from '@vue-storefront/storefront-boilerplate-sdk';
 import { createSharedComposable } from '@vueuse/core';
 
 export const useSdk = createSharedComposable(() => {
+  const config = useRuntimeConfig();
+  const { NUXT_PUBLIC_SDK_SAPCC_API_URL, NUXT_PUBLIC_SDK_SAPCC_SSR_API_URL, NUXT_PUBLIC_SDK_CNTF_API_URL } =
+    config.public;
+
   const sdkConfig = {
     commerce: buildModule<SdkModule>(sdkModule),
+    sapcc: buildModule<SAPCCModuleType>(sapccModule, {
+      apiUrl: NUXT_PUBLIC_SDK_SAPCC_API_URL,
+      ssrApiUrl: NUXT_PUBLIC_SDK_SAPCC_SSR_API_URL,
+    }),
+    contentful: buildModule<ContentfulModuleType>(contentfulModule, {
+      apiUrl: NUXT_PUBLIC_SDK_CNTF_API_URL,
+    }),
   };
 
   return initSDK<typeof sdkConfig>(sdkConfig);
