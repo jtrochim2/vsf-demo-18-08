f9f831fed793a36785715c7e00eff9348c13ff01
daaf <44862757+dawid-ziobro@users.noreply.github.com>
2023-08-30 16:18:04 +0200

    fix: update server hostname to support ipv6 (#28)

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,7 +5,7 @@ import config from '../middleware.config';
 
 (async () => {
   const app = await createServer({ integrations: config.integrations });
-  const host = process.argv[2] ?? 'localhost';
+  const host = process.argv[2] ?? '::';
   const port = Number(process.argv[3]) || 8181;
   const CORS_MIDDLEWARE_NAME = 'corsMiddleware';
 
